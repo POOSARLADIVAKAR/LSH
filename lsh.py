c0980a8ac53497d796e153d7534572ff09bf11c3
Divakar <divakarpoosarla123@gmail.com>
2019-10-20 00:16:04 +0530

    preprocess done on ubuntu

--- lsh.py
+++ lsh.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 def bands_rows(n_doc):
     factors=[]
-    for i in range(1,(int)(math.sqrt(n_doc)+1)):
+    for i in range(1,(int)((n_doc)+1)):
         if n_doc%i==0:
             factors.append((i,n_doc//i))
     return factors
@@ -42,7 +42,7 @@ def lsh(signatureDf, query, jscore,n_doc):
                 signature_tup = (tuple)(signatureDf.iloc[count : count+rows][signatureDf.columns[k]])
                 print(signature_tup)
                 hash_val = hash(signature_tup)
-                hash_val = 1896222623651181975
+                # hash_val = 1896222623651181975
 
                 if hash_val in bucket_dictonary:
                    bucket_dictonary[hash_val].append(k)
@@ -53,7 +53,7 @@ def lsh(signatureDf, query, jscore,n_doc):
             query_tup = tuple(signatureDf.iloc[count:count+rows][query])
             query_hash_val = hash(query_tup)
             print(query_hash_val)
-            query_hash_val = 1896222623651181975
+            # query_hash_val = 1896222623651181975
             
             for k in range(len(bucket_dictonary[query_hash_val])):  #here k is elements in bucket so document accessed by sigDf[ bucket_dic[query_hash_val][k] ]
                 file = signatureDf.columns[bucket_dictonary[query_hash_val][k]]
@@ -68,14 +68,18 @@ def lsh(signatureDf, query, jscore,n_doc):
         # return
         # similar_docs = (set)(similar_docs)
         print(similar_docs)
-        # evaluation(similar_docs)
+        # evaluation(similar_docs.keys)
     # return evaluated_similar_docs
+
+def evaluation(similar_docs,query):
+    # shingleDF = pd.read_pickle("./shingle_pickle.py")
+    similarity_dict = dict()
+    
 
 
 if __name__=='__main__':
     un_pickle_df=pd.read_pickle("./signature_pickle.py")
-    # print(un_pickle_df)
-    similar_documents = lsh(un_pickle_df, "file2", 1,40)
+    print(un_pickle_df)
+    
+    # similar_documents = lsh(un_pickle_df, "file2", 1,40)
     # evaluation
-
-
--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,38 @@
 import pandas as pd
 import os
 import re
+import time
+from os import walk
 
 def get_file_list(dir):
     files = os.listdir(dir)
-    files.reverse()
+    # files.reverse()
+    print(files)
     return files
+
+    
 
 def preprocess():
     file_list = get_file_list('./temp')
+    print(file_list)
     for i in range(len(file_list)):
         file_path='./temp/'+file_list[i]
-        file=open(file_path,"r+")
-        file_data=file.read()
-
-        file_data=file_data.lower()
-        file_data=re.sub(r'[,.\n\s \r\t]',"",file_data)
-
-        file.seek(0)
-        file.write(file_data)
-        file.truncate()
-        file.close()
+        with open(file_path, encoding="utf8", mode="r+" ,errors='ignore') as file:
+            print(file)
+            file_data = file.read()
+            file_data=file_data.lower()
+            print(file_data[0])
+            file_data = re.sub(r'[,.\'"()\n\s \r\t?;:-`”‘’“-]',"",file_data)
+            # print(file_data)
+            file.seek(0)
+            file.write(file_data)
+            file.truncate()
+        # return
         
 if __name__=='__main__':
+    time_start=time.time()
+    # get_file_list("./temp")
     preprocess()
+    time_end=time.time()
+    print(time_end-time_start)
+    
